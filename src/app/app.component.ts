b70d64be48d3cc6c0a860b951f330360dd378bbb
IDOMATH <btdothemath@gmail.com>
2025-02-05 14:58:13 -0600

    Created tasks component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+  selectedUserName!: string;
+
+  onSelectUser(id: string) {
+    this.selectedUserName = DUMMY_USERS.find((user) => user.id === id)!.name;
+    console.log('Selected user with ID: ' + id);
+  }
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,32 @@
-import { Component, signal, computed } from '@angular/core';
+import {
+  Component,
+  computed,
+  EventEmitter,
+  Input,
+  input,
+  Output,
+} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  onSelectUser() {  }
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 }
